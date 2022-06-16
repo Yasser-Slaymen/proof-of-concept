14bb21a41d958a8383e0e34fdbfd883a92695449
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-06-16 14:53:44 +0200

    sryling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const BasUrl = 'https://codingthecurbs.api.fdnd.nl/v1/smartzone'
 const PORT = process.env.PORT || 7000
 app.listen(PORT, () => {console.log(`server running op port ${PORT}`)})
 
