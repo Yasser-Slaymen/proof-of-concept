67bcbc2ad4e3a7f7e048480a22e5d6f62b5869d8
ju5tu5 <justus@ju5tu5.nl>
2022-06-17 11:41:24 +0200

    Voorbeeld Justus

--- main.js
+++ main.js
@@ -3,19 +3,21 @@ const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
 const BasUrl = 'https://codingthecurbs.api.fdnd.nl/v1/smartzone'
-const PORT = process.env.PORT || 7000
-app.listen(PORT, () => {console.log(`server running op port ${PORT}`)})
+const PORT = process.env.PORT || 4003
+app.listen(PORT, () => {
+  console.log(`server running op port ${PORT}`)
+})
 
 // static
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 // Template engine
-app.set('view engine','ejs')
-app.set('views','./views')
+app.set('view engine', 'ejs')
+app.set('views', './views')
 
 // Routes
 const homeRoute = require('./routes/home')
 const addRoute = require('./routes/add')
 
-app.use('/',homeRoute)
-app.use('/',addRoute)
+app.use('/', homeRoute)
+app.use('/', addRoute)
