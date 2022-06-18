bf21ab1f420fe40808288ad344054557eb53554f
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-06-18 03:52:21 +0200

    article page edit

--- routes/article.js
+++ routes/article.js
@@ -1,9 +1,33 @@
 const express = require('express')
 const router = express.Router()
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const BasUrl = 'https://codingthecurbs.api.fdnd.nl/v1/smartzone'
 
-router.get('/article', (request,response) => {
-    response.render('pages/article')
+router.get('/article/:smartzoneId', (request, response) => {
+    fetchJson(`${BasUrl}/${request.params.smartzoneId}`).then(function (jsonData) {
+      response.render('pages/article', {
+        title: 'Voorbeeld titel',
+        data: jsonData.data[0],
+      })
+    })
+  })
 
-})
 
-module.exports= router
+async function fetchJson(url, postData = {}) {
+   return await fetch(url, postData)
+   .then((response) => response.json())
+   .catch((error) => error)
+}
+
+module.exports = router
+
+// router.get('/article', (request, response) => {
+//     fetchJson(BasUrl).then((JsonData) =>{
+//                console.log(JsonData)
+//        response.render('pages/article',{
+//           title:'More From FDND',
+//           smartzones: JsonData.data,
+//        })
+ 
+//     })
+//  })
