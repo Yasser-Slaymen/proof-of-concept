31508773abb7066d41befd9309964c5edc100f1a
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-06-25 01:13:00 +0200

    loading stats & javascript modules edit

---
+++ public/js/loading.js
@@ -0,0 +1,5 @@
+ window.addEventListener("load", ()=> {
+        const spinnerWrapper = document.querySelector(".spinner-wrapper")
+        spinnerWrapper.classList.add("loader");
+    })
+// export default loading
---
+++ public/js/scripts.js
@@ -0,0 +1,1 @@
+import * as loading from '../js/loading.js'
