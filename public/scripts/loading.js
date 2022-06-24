93a5165fa8893793c1b50ba786e6277930f2f89b
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-06-24 15:20:00 +0200

    loading

--- public/scripts/loading.js
+++ public/scripts/loading.js
@@ -1,28 +1,15 @@
 let spinnerWrapper = document.querySelector('.spinner-wrapper')
-setTimeout(() =>{
-    hidePreloader();
-}, 100)
+window.addEventListener('load', () =>{
+    spinnerWrapper.parentElement.removeChild(spinnerWrapper);
+    })
+// setTimeout(() =>{
+//     hidePreloader();
+// }, 10)
 
-function hidePreloader() {
-    spinnerWrapper.style.display = "none"
-}
-let counts = setInterval(updated, 40)
-let upto = 0;
-let width = 0;
-function updated() {
-    const count = document.querySelector('#counter')
-    const hr = document.querySelector('#hr')
-    ++upto;
-    count.innerHTML = "LOADING" + upto + "%"
-    hr.style.width = upto + "%"
-    if(upto ===100) {
-        clearInterval(counts)
-        count.innerHTML = "DONE"
-    }
-}
+// function hidePreloader() {
+//     spinnerWrapper.style.display = "none"
+// }
 
 
 
-// window.addEventListener('load', () =>{
-// spinnerWrapper.parentElement.removeChild(spinnerWrapper);
-// })
+
