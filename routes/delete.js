5b1ab8a1a9dfb4fcc1e095bfd271bc290fac5573
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-07-05 02:02:10 +0200

    Update delete.js

--- routes/delete.js
+++ routes/delete.js
@@ -3,20 +3,18 @@ const router = express.Router()
 const bodyParser = require('body-parser')
 const urlencodedParser = bodyParser.urlencoded({extended:false})
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-// const BasUrl = 'https://codingthecurbs.api.fdnd.nl/v1/smartzone'
 const BasUrl = 'https://blog.api.fdnd.nl/v1/post'
 
 
 router.post('/delete', urlencodedParser,(request,response) => {
     const postData = {
-        method:'delete',
+        method:'DELETE',
         body:JSON.stringify(request.body),
         headers:  {'Content-Type': 'application/json'}
     }
     fetchJson(BasUrl, postData).then(() =>{
         response.render('pages/delete.ejs')
         console.log(postData)
-
     })
     
 })
